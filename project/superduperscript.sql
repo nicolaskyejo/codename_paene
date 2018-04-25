e39d156d3aa5018554c8e6309899ad1d191d0965
nk <root@Nicolas-PC.localdomain>
2018-04-25 19:10:47 +0300

    sql added stuff

--- project/superduperscript.sql
+++ project/superduperscript.sql
@@ -61,25 +61,25 @@ CREATE TABLE Item
 
 
 ############################################################################
-###################  INPUT BELOW       #####################################
+###################  INPUT BELOW  ##########################################
 
 # [Protagonist] #
 INSERT INTO Protagonist VALUES (1);
 
 # [Room] # 33 rooms
-INSERT INTO Room VALUES(100);	#100 and 110 are reserved for corridors
-INSERT INTO Room VALUES(110);
-INSERT INTO Room VALUES(101);	#rooms are numbered clockwise starting from bottom
-INSERT INTO Room VALUES(102);
-INSERT INTO Room VALUES(103);
-INSERT INTO Room VALUES(104);
-INSERT INTO Room VALUES(105);
-INSERT INTO Room VALUES(106);
-INSERT INTO Room VALUES(107);
-INSERT INTO Room VALUES(108);
+INSERT INTO Room VALUES(100,"Clinic/West wing");	#100 and 110 are reserved for corridors
+INSERT INTO Room VALUES(110,"Clinic/Lobby");
+INSERT INTO Room VALUES(101,"Operating room");	#rooms are numbered clockwise starting from bottom
+INSERT INTO Room VALUES(102,NULL);
+INSERT INTO Room VALUES(103,NULL);
+INSERT INTO Room VALUES(104,NULL);
+INSERT INTO Room VALUES(105,NULL);
+INSERT INTO Room VALUES(106,NULL);
+INSERT INTO Room VALUES(107,NULL);
+INSERT INTO Room VALUES(108,NULL);
 
-INSERT INTO Room VALUES(200,NULL);
-INSERT INTO Room VALUES(210,NULL);
+INSERT INTO Room VALUES(200,"Physiotherapy");
+INSERT INTO Room VALUES(210,"Cardiology");
 INSERT INTO Room VALUES(201,NULL);
 INSERT INTO Room VALUES(202,NULL);
 INSERT INTO Room VALUES(203,NULL);
@@ -88,19 +88,19 @@ INSERT INTO Room VALUES(205,NULL);
 INSERT INTO Room VALUES(206,NULL);
 INSERT INTO Room VALUES(207,NULL);
 
-INSERT INTO Room VALUES(300,NULL);
-INSERT INTO Room VALUES(310,NULL);
+INSERT INTO Room VALUES(300,"Oncology");
+INSERT INTO Room VALUES(310,"Radiology");
 INSERT INTO Room VALUES(301,NULL);
 INSERT INTO Room VALUES(302,NULL);
 INSERT INTO Room VALUES(303,NULL);
 INSERT INTO Room VALUES(304,NULL);
-INSERT INTO Room VALUES(305,NULL);   # has 1 npc and 1 enemy
-INSERT INTO Room VALUES(306,NULL);	# has two enemies
+INSERT INTO Room VALUES(305,"Operating room");   # has 1 npc and 1 enemy
+INSERT INTO Room VALUES(306,NULL);	 			 # has two enemies
 INSERT INTO Room VALUES(307,NULL);
 
-INSERT INTO Room VALUES(400,NULL);
-INSERT INTO Room VALUES(410,NULL);
-INSERT INTO Room VALUES(401,NULL);
+INSERT INTO Room VALUES(400,"Neurology");
+INSERT INTO Room VALUES(410,"Orthopaedics");
+INSERT INTO Room VALUES(401,"");			
 INSERT INTO Room VALUES(402,NULL);
 INSERT INTO Room VALUES(403,NULL);
 
@@ -119,7 +119,7 @@ INSERT INTO Npc VALUES(10,"Oliver Cromwell",403,"desc","conv");
 INSERT INTO Npc VALUES(11,"Doctor Reinhold Eisenberg",305,"An old man in his sixties, gaunt and serious looking","Leave me alone for now");       #in room with npc(Paul & follower)
 INSERT INTO Npc VALUES(12,"Paul",305,"A thin man is spread on the operating table","conv");
 
-# [Texti] # Room text  #33 rooms therefore 33 texts
+# [Texti] # Room inside text  #33 rooms therefore 33 texts
 INSERT INTO Texti VALUES("Looks like some kind of corridor", 100);
 INSERT INTO Texti VALUES("Looks like some kind of corridor", 110);
 INSERT INTO Texti VALUES("",101); 
@@ -130,6 +130,36 @@ INSERT INTO Texti VALUES("",105);
 INSERT INTO Texti VALUES("",106); 
 INSERT INTO Texti VALUES("",107);
 INSERT INTO Texti VALUES("",108);
+
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 200);
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 210);
+INSERT INTO Texti VALUES("",201); 
+INSERT INTO Texti VALUES("",202); 
+INSERT INTO Texti VALUES("",203); 
+INSERT INTO Texti VALUES("",204); 
+INSERT INTO Texti VALUES("",205); 
+INSERT INTO Texti VALUES("",206); 
+INSERT INTO Texti VALUES("",207);
+
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 300);
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 310);
+INSERT INTO Texti VALUES("",301); 
+INSERT INTO Texti VALUES("",302); 
+INSERT INTO Texti VALUES("",303); 
+INSERT INTO Texti VALUES("",304); 
+INSERT INTO Texti VALUES("",305); 
+INSERT INTO Texti VALUES("",306); 
+INSERT INTO Texti VALUES("",307);
+
+
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 400);
+INSERT INTO Texti VALUES("Looks like some kind of corridor", 410);
+INSERT INTO Texti VALUES("",401); 
+INSERT INTO Texti VALUES("",402); 
+INSERT INTO Texti VALUES("",403); 
+
+
+
 
 
 # [Room_List] # Each room has a list of room it is connected to
@@ -189,7 +219,7 @@ INSERT INTO Room_List VALUES (305,300);
 
 INSERT INTO Room_List VALUES (306,310);
 INSERT INTO Room_List VALUES (307,310);
-INSERT INTO Room_List VALUES (300,310);
+INSERT INTO Room_List VALUES (300,310);		#corridors
 
 INSERT INTO Room_List VALUES (400,401);
 
@@ -199,7 +229,7 @@ INSERT INTO Room_List VALUES (410,403);
 INSERT INTO Room_List VALUES (401,400);
 INSERT INTO Room_List VALUES (402,410);
 INSERT INTO Room_List VALUES (403,410);
-INSERT INTO Room_List VALUES (400,410);
+INSERT INTO Room_List VALUES (400,410);		#corridors
 
 # [Item] # 9 items
 INSERT INTO Item VALUES (6, "Scalpel", 0, NULL, 101, "sharp, a tool used in surgery");
