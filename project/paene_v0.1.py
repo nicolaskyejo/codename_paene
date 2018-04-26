fc4c98e036784e85fad1b6802d49160f9844ff4f
lauriuo <lauri.a.uotila@gmail.com>
2018-04-26 10:05:18 +0300

    added print function

--- project/paene_v0.1.py
+++ project/paene_v0.1.py
@@ -49,9 +49,9 @@ def room_description(room: int, database=db):
         cursor.execute(query)
         description = cursor.fetchone()
 
+        return description[0]
     except mysql.connector.Error as e:
         print(e)
-    return description[0]
 
 
 """ Shows the text of the room the player is in, and the rooms that can be entered from there."""
@@ -67,15 +67,15 @@ def show_room(current_room: int, database=db):
         for text in cursor.fetchone():
             showable_text = showable_text + text
 
-        showable_text += "\nI can enter rooms: "
+        showable_text += "\nI can enter rooms:\n"
         for room_num in available_rooms:
             door_description = room_description(room_num)
 
             if door_description == None:
-                showable_text += str(room_num) + ", "
+                showable_text += str(room_num) + "\n"
 
             else:
-                showable_text += str(door_description) + ", "
+                showable_text += str(room_num) + ": " + str(door_description) + "\n"
 
     except mysql.connector.Error as e:
         print(e)
@@ -85,7 +85,25 @@ def show_room(current_room: int, database=db):
 
     return showable_text
 
+"""Vesa's print function for formating printed text"""
 
+def our_print(textline):
+    line_length = 80
+    list_of_words = textline.split()
+    used = 0
+
+    for word in list_of_words:
+        if used + len(word) <= line_length:
+            if used > 0:
+                print(" ", end="")
+            print(word, end="")
+
+        else:
+            print("")
+            used = 0
+            print(word, end="")
+        used = used + len(word)
+    print("")
 
 
 # # # [MAIN PROGRAM] # # #
@@ -94,5 +112,5 @@ def show_room(current_room: int, database=db):
 #print(room_list_returner(100))
 
 print(show_room(100))
-
+our_print("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
 db.rollback()
