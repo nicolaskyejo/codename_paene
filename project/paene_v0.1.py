4c02cf5b4566bc35f999e0042471c5382b79e825
lauriuo <lauri.a.uotila@gmail.com>
2018-04-27 10:48:10 +0300

    __init__ file, import from other files

--- project/paene_v0.1.py
+++ project/paene_v0.1.py
@@ -6,25 +6,22 @@
 #  Copyright 2018 Oliver, Nicolas & Lauri
 #
 ##### PREAMBLE #####  
-import gamefuncs
-import queryfuncs
-
-
+from gamefuncs import *
+from queryfuncs import *
 
 # # # [MAIN PROGRAM] # # #
 if __name__ == '__main__':
 
-    print(queryfuncs.show_room(101))
+    print(show_room(101))
     current_room = 101
     while True:
-        players_input = gamefuncs.get_user_input(str(input()))
-        if players_input[0] in gamefuncs.commands():
+        players_input = get_user_input(str(input()))
+        if players_input[0] in commands():
 
-            current_room = gamefuncs.go(current_room, players_input[1])
-            print(queryfuncs.show_room(current_room))
+            current_room = go(current_room, players_input[1])
+            print(show_room(current_room))
         else:
             print("huh wat u say man")
-
 
 #print(room_list_returner(100))
 #print(show_room(100))
